103a93a5fecf9892da30a8181afa613ceb2dad56
stepin104865 <69194460+stepin104865@users.noreply.github.com>
2021-02-04 18:32:46 +0530

    incsrc

---
+++ Activity-2b/header/satya.h
@@ -0,0 +1,8 @@
+#ifndef __SATYA_H__
+#define __SATYA_H__
+
+#include<stdio.h>
+
+ int satya();
+
+ #endif
---
+++ Activity-2b/src/satya.c
@@ -0,0 +1,7 @@
+#include<satya.h>
+
+int satya()
+{
+    printf("i'm satya");
+    return 0;
+}
